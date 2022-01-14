0318e3e5559c2103f2464cde384d024f11abc69e
stui <me@stui.ch>
2022-01-14 02:49:10 +0100

    🛍 added food and topup endpoint

--- src/dist/v1/products/topup.php
+++ src/dist/v1/products/topup.php
@@ -1,1 +1,30 @@
 <?php
+
+    declare(strict_types=1);
+
+    use IOL\Shop\v1\BitMasks\RequestMethod;
+    use IOL\Shop\v1\Request\APIResponse;
+
+    $response = APIResponse::getInstance();
+
+    $response->setAllowedRequestMethods(
+        new RequestMethod(RequestMethod::GET)
+    );
+    $response->needsAuth(true);
+    $userID = $response->check();
+
+    $category = new \IOL\Shop\v1\Entity\Category(3);
+    $category->loadProducts();
+
+    foreach($category->getProducts() as $product){
+        $data = [
+            'id' => $product->getId(),
+            'title' => $product->getTitle(),
+            'dateFrom' => $product->getAdditionalData()['dateFrom'],
+            'dateTo' => $product->getAdditionalData()['dateTo'],
+            'location' => $product->getAdditionalData()['location'],
+            'price' => $product->getPrice(),
+        ];
+
+        $response->setData($data);
+    }
