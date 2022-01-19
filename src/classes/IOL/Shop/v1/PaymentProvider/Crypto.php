53989aa47a4c870faf1508fce8edcb30d480a7e2
stui <me@stui.ch>
2022-01-19 11:47:02 +0100

    testing coinbase api

--- src/classes/IOL/Shop/v1/PaymentProvider/Crypto.php
+++ src/classes/IOL/Shop/v1/PaymentProvider/Crypto.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace IOL\Shop\v1\PaymentProvider;
 
+use CoinbaseCommerce\ApiClient;
+use CoinbaseCommerce\Resources\Checkout;
+use IOL\Shop\v1\DataSource\Environment;
 use IOL\Shop\v1\Entity\Order;
 
 class Crypto extends PaymentProvider implements PaymentProviderInterface
@@ -18,6 +21,22 @@ class Crypto extends PaymentProvider implements PaymentProviderInterface
 
     public function createPayment(Order $order): string
     {
+        ApiClient::init(Environment::get('COINBASE_SECRET'));
+
+        $checkoutData = [
+            'name' => 'Bestellung bei Isle of LAN',
+            'description' => 'Bestell-Nr. '.$order->getId(),
+            'pricing_type' => 'fixed_price',
+            'local_price' => [
+                'amount' => number_format(($order->getTotal() + $order->getFees()), 2, '.', ''),
+                'currency' => 'CHF'
+            ],
+            'requested_info' => ['name', 'email']
+        ];
+        $newCheckoutObj = Checkout::create($checkoutData);
+
+        var_dump($newCheckoutObj);
+
         return '';
     }
 
