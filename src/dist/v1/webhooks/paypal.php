250f787df489bbff8e8920b3ef742f61d182948c
stui <stui@isleoflan.ch>
2022-01-21 01:40:14 +0100

    started dev on paypal webhook

--- src/dist/v1/webhooks/paypal.php
+++ src/dist/v1/webhooks/paypal.php
@@ -12,6 +12,10 @@ $headers = getallheaders();
 
 $headers = array_change_key_case($headers, CASE_UPPER);
 $apiContext = new \PayPal\Rest\ApiContext(new \PayPal\Auth\OAuthTokenCredential(Environment::get('PAYPAL_ID_'.Environment::get('PAYMENT_MODE')), Environment::get('PAYPAL_SECRET_'.Environment::get('PAYMENT_MODE'))));
+if (Environment::get('PAYMENT_MODE') == 'live'){
+    $apiContext->setConfig(['mode' => 'live']);
+}
+
 $signatureVerification = new VerifyWebhookSignature();
 $signatureVerification->setAuthAlgo($headers['PAYPAL-AUTH-ALGO']);
 $signatureVerification->setTransmissionId($headers['PAYPAL-TRANSMISSION-ID']);
@@ -31,18 +35,44 @@ try {
 }
 if($output->getVerificationStatus() === 'SUCCESS') {
     // signature is valid
-    file_put_contents('/var/www/stripe.txt', $requestBody);
 
     $data = json_decode($requestBody, true);
-    if($data['resource']['state'] === 'completed'){
+    $orderId = $data['resource']['transactions'][0]['invoice_number'];
+    $paymentId = $data['resource']['id'];
+    $payerId = $data['resource']['payer']['payer_info']['payer_id'];
 
-    }
-/*
-    $orderId = $object->client_reference_id;
 
     try {
         $order = new \IOL\Shop\v1\Entity\Order($orderId);
     } catch (\IOL\Shop\v1\Exceptions\IOLException) {}
+
+
+    $keys = $this->getAPIKey('paypal');
+
+    $payment = \PayPal\Api\Payment::get($paymentId, $apiContext);
+
+    $execution = new \PayPal\Api\PaymentExecution();
+    $execution->setPayerId($payerId);
+
+
+    $total = $order->getTotal() / 100;
+
+
+    $transaction = new \PayPal\Api\Transaction();
+    $amount = new \PayPal\Api\Amount();
+    $details = new \PayPal\Api\Details();
+
+    $details->setSubtotal(number_format($total,2,".","'"));
+
+    $amount->setCurrency('CHF');
+    $amount->setTotal(number_format($total, 2,".","'"));
+    $amount->setDetails($details);
+    $transaction->setAmount($amount);
+    $execution->addTransaction($transaction);
+
+    try {
+        $result = $payment->execute($execution, $apiContext);
+    } catch(Exception $e){}
 
     $invoice = new \IOL\Shop\v1\Entity\Invoice();
     $invoice->getForOrder($order);
@@ -51,7 +81,8 @@ if($output->getVerificationStatus() === 'SUCCESS') {
     $order->sendConfirmationMail();
 
     $order->completeOrder();
-    */
+    http_response_code(200);
+
 } else {
     http_response_code(400);
 }
