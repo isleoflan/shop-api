929c053a62d15af4f04fa6e69d855096c91084bd
stui <stui@isleoflan.ch>
2022-01-21 02:23:03 +0100

    started dev on coinbase webhook

--- src/dist/v1/webhooks/coinbase.php
+++ src/dist/v1/webhooks/coinbase.php
@@ -8,9 +8,6 @@ use IOL\Shop\v1\DataSource\Environment;
 error_reporting(E_ALL);
 
 
-$payload = file_get_contents('php://input');
-$signatureHeader = $_SERVER['HTTP_STRIPE_SIGNATURE'];
-$event = null;
 
 $headerName = 'X-Cc-Webhook-Signature';
 $headers = getallheaders();
@@ -21,6 +18,7 @@ try {
     $event = Webhook::buildEvent(trim(file_get_contents('php://input')), $headers[$headerName] ?? null, Environment::get("COINBASE_SECRET"));
     http_response_code(200);
 
+    throw new Exception(var_export($event, true));
     switch($event->type){
         case 'charge:confirmed':
         case 'charge:delayed':
@@ -45,28 +43,4 @@ try {
     echo $exception->getMessage();
     echo 'Failed';
 }
-
-
-$object = $event->data->object;
-
-if($event->type === 'checkout.session.completed') {
-        /** @var $object \Stripe\Checkout\Session */
-
-        if($object->status === 'complete') {
-            $orderId = $object->client_reference_id;
-
-            try {
-                $order = new \IOL\Shop\v1\Entity\Order($orderId);
-            } catch (\IOL\Shop\v1\Exceptions\IOLException) {}
-
-            $invoice = new \IOL\Shop\v1\Entity\Invoice();
-            $invoice->getForOrder($order);
-
-            $invoice->createPayment($invoice->getValue());
-            $order->sendConfirmationMail();
-
-            $order->completeOrder();
-        }
-}
-
 http_response_code(200);
