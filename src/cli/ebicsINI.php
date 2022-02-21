7a9ea4c12d919ab6d972ecf782954b4da8565804
stui <stui@isleoflan.ch>
2022-02-21 11:30:51 +0100

    changed INI request to support multiple accounts

--- src/cli/ebicsINI.php
+++ src/cli/ebicsINI.php
@@ -17,54 +17,52 @@ for ($returnDirs = 0; $returnDirs < 1; $returnDirs++) {
 
 require_once $basePath . '/_loader.php';
 
+foreach (['Private', 'Company'] as $userType) {
+    $keyRingRealPath = File::getBasePath() . '/assets/ebics/ebics' . $userType . 'Keyring.json';
+    $keyRingManager = new KeyRingManager($keyRingRealPath, Environment::get('EBICS_KEYRING_PASSPHRASE'));
+    $keyRing = $keyRingManager->loadKeyRing();
 
-$keyRingRealPath = File::getBasePath().'/assets/ebics/ebicsCompanyKeyring.json';
-$keyRingManager = new KeyRingManager($keyRingRealPath, Environment::get('EBICS_KEYRING_PASSPHRASE'));
-$keyRing = $keyRingManager->loadKeyRing();
+    $bank = new Bank(Environment::get('EBICS_HOST_ID'), Environment::get('EBICS_URL'), Bank::VERSION_30);
+    $bank->setIsCertified(false);
+    $user = new User(Environment::get('EBICS_' . strtoupper($userType) . '_PARTNER_ID'), Environment::get('EBICS_USER_ID'));
+    $client = new EbicsClient($bank, $user, $keyRing);
 
-$bank = new Bank(Environment::get('EBICS_HOST_ID'), Environment::get('EBICS_URL'), Bank::VERSION_30);
-$bank->setIsCertified(false);
-$user = new User(Environment::get('EBICS_COMPANY_PARTNER_ID'), Environment::get('EBICS_USER_ID'));
-$client = new EbicsClient($bank, $user, $keyRing);
+    try {
+        $client->INI();
+        $keyRingManager->saveKeyRing($keyRing);
+    } catch (EbicsResponseExceptionInterface $exception) {
+        echo sprintf(
+            "INI request failed. EBICS Error code : %s\nMessage : %s\nMeaning : %s",
+            $exception->getResponseCode(),
+            $exception->getMessage(),
+            $exception->getMeaning()
+        );
+    }
 
+    try {
+        $client->HIA();
+        $keyRingManager->saveKeyRing($keyRing);
+    } catch (EbicsResponseExceptionInterface $exception) {
+        echo sprintf(
+            "HIA request failed. EBICS Error code : %s\nMessage : %s\nMeaning : %s",
+            $exception->getResponseCode(),
+            $exception->getMessage(),
+            $exception->getMeaning()
+        );
+    }
 
-try {
     /* @var \AndrewSvirin\Ebics\EbicsClient $client */
-    $client->INI();
-    /* @var \AndrewSvirin\Ebics\Services\KeyRingManager $keyRingManager */
-    /* @var \AndrewSvirin\Ebics\Models\KeyRing $keyRing */
-    $keyRingManager->saveKeyRing($keyRing);
-} catch (EbicsResponseExceptionInterface $exception) {
-    echo sprintf(
-        "INI request failed. EBICS Error code : %s\nMessage : %s\nMeaning : %s",
-        $exception->getResponseCode(),
-        $exception->getMessage(),
-        $exception->getMeaning()
+    $ebicsBankLetter = new \AndrewSvirin\Ebics\EbicsBankLetter();
+
+    $bankLetter = $ebicsBankLetter->prepareBankLetter(
+        $client->getBank(),
+        $client->getUser(),
+        $client->getKeyRing()
     );
+
+    $txt = $ebicsBankLetter->formatBankLetter($bankLetter, $ebicsBankLetter->createPdfBankLetterFormatter());
+    file_put_contents(File::getBasePath() . '/assets/ebics/INILetter' . $userType . '.pdf', $txt);
+
+    \IOL\Shop\v1\Request\APIResponse::getInstance()->addData('pdf', $txt);
+
 }
-
-try {
-    $client->HIA();
-    $keyRingManager->saveKeyRing($keyRing);
-} catch (EbicsResponseExceptionInterface $exception) {
-    echo sprintf(
-        "HIA request failed. EBICS Error code : %s\nMessage : %s\nMeaning : %s",
-        $exception->getResponseCode(),
-        $exception->getMessage(),
-        $exception->getMeaning()
-    );
-}
-
-/* @var \AndrewSvirin\Ebics\EbicsClient $client */
-$ebicsBankLetter = new \AndrewSvirin\Ebics\EbicsBankLetter();
-
-$bankLetter = $ebicsBankLetter->prepareBankLetter(
-    $client->getBank(),
-    $client->getUser(),
-    $client->getKeyRing()
-);
-
-$txt = $ebicsBankLetter->formatBankLetter($bankLetter, $ebicsBankLetter->createPdfBankLetterFormatter());
-file_put_contents(File::getBasePath().'/assets/ebics/companyINIletter.pdf', $txt);
-
-\IOL\Shop\v1\Request\APIResponse::getInstance()->addData('pdf',$txt);
